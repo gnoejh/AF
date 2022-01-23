e0f603cc602a3521ff4df407aaa095db559f9ae9
gnoejh <hjeong@postech.ac.kr>
2022-01-23 15:00:52 +0500

    init

---
+++ Total/Exercise1.03/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //TODO 1
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        setContentView(webView)
+        webView.loadUrl("https://www.google.com")
+    }
+}
